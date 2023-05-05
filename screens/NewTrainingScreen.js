700df84b47122e8c3f561512465dcbb9337de75c
Sebastián Capelli <scapelli@fi.uba.ar>
2023-05-05 00:55:23 -0300

    searchbar con dropdown para busqueda de tipo de entrenamiento y añado clase levelInput TBD

--- screens/NewTrainingScreen.js
+++ screens/NewTrainingScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Text } from 'react-native';
-import { DaysInput, DividerWithLeftText, TextBox, ButtonStandard, ConfirmationButtons } from '../src/styles/BaseComponents';
+import { DividerWithLeftText, TextBox } from '../src/styles/BaseComponents';
 import styles from '../src/styles/styles';
+import { SelectList } from 'react-native-dropdown-select-list'
+import LevelInput from '../src/components/LevelInput';
 
-import { Searchbar } from 'react-native-paper';
 
-const trainingTypes = ['Fuerza','Resistencia','Flexibilidad','Velocidad','Agilidad','Equilibrio','Coordinación','Potencia','Precisión','Fuerza explosiva','Fuerza reactiva','Fuerza resistencia','Fuerza máxima','Fuerza velocidad','Resistencia aeróbica','Resistencia anaeróbica','Resistencia de fuerza','Running','Ciclismo','Natación','Triatlón','Crossfit','Calistenia','Gimnasia','Yoga','Pilates','Artes marciales','Deportes de raqueta','Deportes de equipo',]
 
 export default class NewTrainingScreen extends Component {
     constructor(props) {
@@ -13,10 +13,24 @@ export default class NewTrainingScreen extends Component {
         this.state = {
             title: '',
             description: '',
+            trainingType: '',
+            level: 'basic',
         }
     }
 
+    getTrainingsTypes() {
+
+        // reemplazar por request de tipos de entrenamiento
+        //-----------------------------------------------
+        const trainingTypes = ['Fuerza','Resistencia','Flexibilidad','Velocidad','Agilidad','Equilibrio','Coordinación','Potencia','Precisión','Fuerza explosiva','Fuerza reactiva','Fuerza resistencia','Fuerza máxima','Fuerza velocidad','Resistencia aeróbica','Resistencia anaeróbica','Resistencia de fuerza','Running','Ciclismo','Natación','Triatlón','Crossfit','Calistenia','Gimnasia','Yoga','Pilates','Artes marciales','Deportes de raqueta','Deportes de equipo',]
+        //-----------------------------------------------
+        
+        return trainingTypes
+    }
+
     render() {
+
+        const trainingTypes = this.getTrainingsTypes()
 
         return (
             <ScrollView 
@@ -28,7 +42,7 @@ export default class NewTrainingScreen extends Component {
                 
                 <TextBox 
                     title="Título"
-                    onChangeText={(metric) => this.setState({ title })}
+                    onChangeText={(title) => this.setState({ title })}
                     maxLength={60}
                     style={{
                         marginTop: 5,
@@ -37,57 +51,50 @@ export default class NewTrainingScreen extends Component {
                 
                 <TextBox 
                     title="Descripción"
-                    onChangeText={(metric) => this.setState({ description })}
+                    onChangeText={(description) => this.setState({ description })}
                     maxLength={250}
                     style={{
                         marginTop: 5,
                     }}
                 />
 
-                <SearchWithDropdown />
-
-                <TextBox 
-                    title="Título"
-                    onChangeText={(metric) => this.setState({ title })}
-                    maxLength={60}
+                <DividerWithLeftText
+                    text="Tipo"
                     style={{
                         marginTop: 5,
                     }}
                 />
+
+                <SelectList
+                    setSelected={(trainingType) => this.setState({ trainingType })} 
+                    data={trainingTypes} 
+                    save="value"
+                    placeholder="Tipo de entrenamiento"
+                    notFoundText="No se encontraron resultados"
+                    searchPlaceholder="Buscar"
+                    boxStyles={{borderRadius: 5, width: 350, marginTop: 10}}
+                    inputStyles={{color: 'black'}}
+                />
+
+                <DividerWithLeftText
+                    text="Nivel"
+                    style={{
+                        marginTop: 10,
+                    }}
+                />
+
+                <LevelInput 
+                    initialLevel={this.state.level}
+                    setSelected={(level) => this.setState({ level })}
+                    style={{
+                        marginTop: 5,
+                    }}
+                />
+
+                
             </View>
               
             </ScrollView>
-        );
-    }
-}
-
-class SearchWithDropdown extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            searchQuery: '',
-        }
-    }
-
-    render() {
-        const { searchQuery } = this.state;
-
-        const filteredTrainingTypes = trainingTypes.filter(trainingType =>
-            trainingType.toLowerCase().includes(searchQuery.toLowerCase())
-          );
-
-        return (
-            <>
-            
-            <Searchbar
-                placeholder="Tipo de entrenamiento"
-                onChangeText={(searchQuery) => this.setState({ searchQuery })}
-                value={searchQuery}
-            />
-            {filteredTrainingTypes.slice(0, 6).map(trainingType => (
-                <Text key={trainingType}>{trainingType}</Text>
-              ))}
-            </>
         );
     }
 }
---
+++ src/components/LevelInput.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Avatar, Button, Card, Text, IconButton } from 'react-native-paper';
+import { View, Image, StyleSheet } from 'react-native';
+
+
+export default class LevelInput extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <View style={[this.props.style,{flexDirection: 'row'}]}>
+                <Text>To be implemented</Text>
+            </View>
+        )               
+    }
+}
+
+const goalsStyles = StyleSheet.create({
+})
