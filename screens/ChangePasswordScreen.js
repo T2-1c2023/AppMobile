9a91a47938a80da582e435fc44cf2c5f103414f3
Sebastián Capelli <scapelli@fi.uba.ar>
2023-05-14 14:37:28 -0300

    se corrigen detalles de estilo de ventana de cambio de contraseña

--- screens/ChangePasswordScreen.js
+++ screens/ChangePasswordScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Keyboard } from 'react-native';
-import { TextInput, Button, Text } from 'react-native-paper';
+import { HelperText, TextInput, Button, Text } from 'react-native-paper';
 import { PinInput } from '../src/components/PinInput'
 import { TextHeader, TextDetails, DividerWithMiddleText, ButtonStandard, InputData, TextWithLink, LoginImage } from '../src/styles/BaseComponents';
 import styles from '../src/styles/styles';
@@ -8,29 +8,53 @@ import styles from '../src/styles/styles';
 export default class ChangePasswordScreen extends Component {
     constructor(props) {
         super(props)
+        this.handleChangePasswordPress = this.handleChangePasswordPress.bind(this)
+        this.confirmPassIsValid = this.confirmPassIsValid.bind(this)
+        this.newPassTextInput = React.createRef()
+        this.ConfirmPassTextInput = React.createRef()
         this.state = {
-            pin: '',
+            currentPass: '',
+            newPass: '',
+            confirmPass: '',
         }
     }
 
+    handleChangePasswordPress() {
+        Keyboard.dismiss();
+        
+        // Reemplazar por la o las requests de cambio de contraseña
+        alert("contraseñas: " + this.state.confirmPass + ' ' + this.state.newPass + ' ' + this.state.currentPass)
+    }
+
+    confirmPassIsValid() {
+        return this.state.newPass === this.state.confirmPass
+    }
+
+    fieldsNotEmpty() {
+        return this.state.currentPass !== '' && this.state.newPass !== '' && this.state.confirmPass !== ''
+    }
+
     render() {
+
         return (
             <View style={ styles.container }>
                 <TextInput
                     label="Constraseña actual"
                     theme={{ colors: { primary: '#21005D'}}}
-                    // onChangeText={text => setText(text)}
+                    onChangeText={text => this.setState({ currentPass: text })}
+                    onSubmitEditing={() => this.newPassTextInput.current.focus()}
                     mode='flat'
                     style={
                         textStyles.inputText
                     }
                     secureTextEntry
-                    
                 />
                 <TextInput
+                    ref={this.newPassTextInput}
                     label="Nueva contraseña"
                     theme={{ colors: { primary: '#21005D'}}}
-                    // onChangeText={text => setText(text)}
+                    onChangeText={text => this.setState({ newPass: text })}
+                    onSubmitEditing={() => this.ConfirmPassTextInput.current.focus()}
                     mode='flat'
                     style={
                         textStyles.inputText
@@ -39,27 +63,31 @@ export default class ChangePasswordScreen extends Component {
                     
                 />
                 <TextInput
-                label="Confirmar contraseña"
-                theme={{ colors: { primary: '#21005D'}}}
-                // onChangeText={texto => setText(text)}
-                mode='flat'
-                style={
-                    textStyles.inputText
-                }
-                secureTextEntry
-                
-            />
-                {/* <InputTextWithUnderline
-                    header="Contraseña actual"
-
-
-                /> */}
+                    ref={this.ConfirmPassTextInput}
+                    label="Confirmar contraseña"
+                    theme={this.confirmPassIsValid() || this.state.confirmPass === ''?
+                        { colors: { primary: '#21005D'}} 
+                        : 
+                        { colors: { primary: 'red'}}}
+                    onChangeText={text => this.setState({ confirmPass: text })}
+                    mode='flat'
+                    style={
+                        textStyles.inputText
+                    }
+                    secureTextEntry
+                />
+                <HelperText 
+                    type="error" 
+                    visible={!this.confirmPassIsValid() && this.state.confirmPass !== ''}
+                    style={textStyles.helperText}
+                >
+                    La nueva contraseña no coincide
+                </HelperText>
                 
                 <ButtonStandard 
-                    onPress={() => { 
-                        console.log('pin: ', this.state.pin) 
-                    }} 
+                    onPress={this.handleChangePasswordPress}
                     title="Cambiar contraseña"
+                    disabled={!this.confirmPassIsValid() || !this.fieldsNotEmpty()}
                     style={{
                         marginTop: 100,
                     }}
@@ -74,19 +102,15 @@ const textStyles = StyleSheet.create({
         backgroundColor: "transparent",
         width: '95%',
         marginTop: 20,
-       color: "black",
+    },
+
+    helperText: {
+        alignSelf: 'flex-start',
+        marginLeft: 15,
+        color: 'red',
     }
 
 })
-    
-// class InputTextWithUnderline extends Component {
-//     render() {
-//         return (
-//             <TextInput
-//             label="Email"
-//             value={text}
-//             onChangeText={text => setText(text)}
-//           />
-//         );
-//     }
-// }
+
+
+
