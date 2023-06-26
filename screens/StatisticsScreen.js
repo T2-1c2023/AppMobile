23c4f472343c240673c4351b039c09c4d03f1092
SCAPELLI <scapelli@fi.uba.ar>
2023-06-25 21:57:10 -0300

    la ventana de estadisticas ahora muestra un timeline y los valores totales de forma prolija

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text, Image } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
 import { DividerWithLeftText, TextBox } from '../src/styles/BaseComponents';
 import styles from '../src/styles/styles';
 import { ConfirmationButtons, ButtonStandard } from '../src/styles/BaseComponents';
@@ -18,6 +18,9 @@ import { UserContext } from '../src/contexts/UserContext';
 import RadiusInput from '../src/components/RadiusInput';
 import jwt_decode from 'jwt-decode';
 
+import Timeline from 'react-native-timeline-flatlist'
+import Icon from 'react-native-paper/src/components/Icon'
+
 const API_GATEWAY_URL = Constants.manifest?.extra?.apiGatewayUrl;
 
 export default class StatisticsScreen extends Component {
@@ -25,16 +28,137 @@ export default class StatisticsScreen extends Component {
 
     constructor(props) {
         super(props)
-    }
-    componentDidMount() {
 
+        this.state = {
+            loading: true,
+            time: 0,
+            calories: 0,
+            distance: 0,
+            goalsAmmount: 0,
+            sessionsAmmount: 0,
+            sessions: [],
+        }
+
+        // this.focusListener = this.props.navigation.addListener('focus', () => {
+        //     this.componentDidMount()
+        // })
+    }
+
+    getSessionsFormatted(sessionsResponse) {
+        return sessionsResponse.map((session) => {
+            let description = 
+                "Tiempo:    " + session.time_spent_mins + " min, " + "\n" +
+                "Calorias:  " + session.calories_burned + " kcal, " + "\n" +
+                "Distancia: " + session.distance_km + " km"
+        
+            return {
+                time: session.date,
+                title: session.training_type_description,
+                description: description,
+            }
+        })
+    }
+
+    componentDidMount() {
+        this.loadStats()
+    }
+
+    loadStats() {
+        this.setState({loading: true})
+
+        let athleteId = this.context.userId
+        let url = API_GATEWAY_URL + "athletes/" + athleteId + "/sessions"
+        let config = {
+            headers: { Authorization: tokenManager.getAccessToken() },
+            params: {}
+        } 
+
+        axios.get(url, config)
+            .then((response) => {
+                console.log("Response: " + JSON.stringify(response.data))
+                let data = response.data
+                let sessionsResponse = response.data.sessions
+                let sessionsFormatted = this.getSessionsFormatted(sessionsResponse)
+                this.setState({
+                    loading: false,
+                    time: data.time_spent_mins_total,
+                    calories: data.calories_burned_total,
+                    distance: data.distance_km_total,
+                    goalsAmmount: data.completed_training_goals,
+                    sessionsAmmount: data.sessions.length,
+                    sessions: sessionsFormatted,
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            }
+        )
+
+    }
+
+    renderStat(title, iconName, value, unit) {
+        return (
+            <View style={statsStyles.statContainer}>
+                <Icon 
+                    source={iconName} 
+                    size={40} 
+                    color='#21005D'/>
+                <Text style={statsStyles.statText}>
+                    {title}: {value} {unit}
+                </Text>
+            </View>
+        )
     }
 
     render() {
-        return (
-            <Text>
-                Hola
-            </Text>
+        return this.state.loading?
+        (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#21005D" />
+                <Text style={{marginTop: 30}}>Cargando estadisticas ...</Text>
+            </View>
+        )
+        :
+        (
+            <View style={{flex:1, backgroundColor: '#DED8E1',}} >
+                <View style={[styles.container, {marginTop: 10, flex: 0.4}]}>
+                    {this.renderStat("Cantidad de sesiones", "counter", this.state.sessionsAmmount, "")}
+                    {this.renderStat("Tiempo consumido", "timer-outline", this.state.time, "min")}
+                    {this.renderStat("Calorías consumidas", "fire", this.state.calories, "kcal")}
+                    {this.renderStat("Distancia recorrida", "map-marker-distance", this.state.distance, "km")}
+                    {this.renderStat("Metas completadas", "check-circle-outline", this.state.goalsAmmount, "")}
+
+                </View>
+                <Timeline
+                    style={statsStyles.timeline}
+                    data={this.state.sessions}
+                    timeContainerStyle={{minWidth:80}}
+                />
+            </View>
         )
     }
 }
+
+const statsStyles = StyleSheet.create({
+    statContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        // backgroundColor: 'green',
+        marginTop: 10,
+        paddingLeft: 20,
+        width: '100%',
+    },
+
+    statText: {
+        marginLeft: 10,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
+    timeline: {
+        flex: 0.6, 
+        backgroundColor: '#DED8E1', 
+        paddingLeft: 30
+    },
+})
